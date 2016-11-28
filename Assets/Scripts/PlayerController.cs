3015c70b9663c5383d44f40441ae4b465146f44a
jannef <janne.forsell@cs.tamk.fi>
2016-11-28 06:17:35 +0200

    First version of step taking script done.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,5 +1,6 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
+using UnityEngine;
+using System.Collections.Generic;
 using UnityEditor;
 
 namespace Stairs
@@ -7,18 +8,57 @@ namespace Stairs
     [RequireComponent(typeof(Rigidbody))]
     public class PlayerController : MonoBehaviour
     {
-        [SerializeField] private Vector3 speed;
+        private enum WaypointType { Step = 0, Walk = 1 }
 
-        private Rigidbody _rb = null;
+        private struct Waypoint
+        {
+            public Vector3 position;
+            public WaypointType type;
+
+            public Waypoint(Vector3 position, WaypointType type = WaypointType.Step)
+            {
+                this.position = position;
+                this.type = type;
+            }
+        }
+
+        [SerializeField] private float TimePerStep = 4.20f;
+        
+        private readonly Queue<Waypoint> _playerPath = new Queue<Waypoint>();
+        private bool _takingStep = false;
 
         private void Awake()
         {
-            _rb = GetComponent<Rigidbody>();
+           
         }
 
         private void Update()
         {
-            transform.Translate(speed * Time.deltaTime);
+            if (!_takingStep) StartCoroutine(WalkToWaypoint(_playerPath.Dequeue().position, TimePerStep));
         }
+
+        public void AddStep(Vector3 destination)
+        {
+            _playerPath.Enqueue(new Waypoint(destination));
+        }
+
+        private IEnumerator WalkToWaypoint(Vector3 wayPoint, float duration)
+        {
+            _takingStep = true;
+            var start = transform.position;
+            var timer = 0f;
+
+            while (timer <= duration)
+            {
+                timer += Time.deltaTime;
+                var ratio = Mathf.Min(1f, timer/duration);
+
+                transform.position = Vector3.Lerp(start, wayPoint, ratio);
+                yield return new WaitForEndOfFrame();
+            }
+
+            _takingStep = false;
+        }
+
     }
 }
--- Assets/Scripts/Stairs.cs
+++ Assets/Scripts/Stairs.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+﻿using System.Collections.Generic;
 using Stairs.Utils;
 using UnityEngine;
 
@@ -11,11 +11,13 @@ namespace Stairs
         private Vector3 _stepSize = Vector3.zero;
         private Vector3 _nextStep = Vector3.zero;
 
+        private PlayerController _player;
+
         private void Awake()
         {
             Pool.Instance.AddToPool(StairPrefab, 25, transform);
             _stepSize = StairPrefab.GetComponent<MeshRenderer>().bounds.size;
-
+            _player = FindObjectOfType<PlayerController>();
 
             for (int i = 0; i < 50; i++)
             {
@@ -27,6 +29,7 @@ namespace Stairs
         {
             var go = Pool.Instance.GetObject(StairPrefab);
             go.transform.position = _nextStep;
+            if (_player != null) _player.AddStep(_nextStep + new Vector3(0, _stepSize.y, 0));
             _nextStep += new Vector3(0, _stepSize.y, _stepSize.z);
         }
     }
